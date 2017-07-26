16646479e2ef2e4cc94f4088c99b47631cda4b7c
Tayo Odedele <odedeletayo@gmail.com>
2017-07-26 12:09:38 -0500

    Updated Version Including Helmet and Compression

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var expressValidator = require('express-validator');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var doctor = require('./routes/doctor');
+var compression  = require('compression');
+var helmet = require('helmet');
 //var registration = require('./routes/registration');
 var app = express();
 
@@ -29,11 +31,13 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
